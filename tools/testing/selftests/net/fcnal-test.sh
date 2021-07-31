3602bf060aae5a7a873a5bcc08aa7debcf26ef48
Leonard Crestez <cdleonard@gmail.com>
2021-07-31 18:32:26 +0300

    selftests: Add -t tcp_authopt option for fcnal-test.sh
    This script is otherwise very slow to run!
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tools/testing/selftests/net/fcnal-test.sh
+++ tools/testing/selftests/net/fcnal-test.sh
@@ -1330,6 +1330,17 @@ ipv4_tcp()
 	ipv4_tcp_vrf
 }
 
+
+only_tcp_authopt()
+{
+	log_section "TCP Authentication"
+	setup
+	set_sysctl net.ipv4.tcp_l3mdev_accept=0
+	log_subsection "IPv4 no VRF"
+	ipv4_tcp_authopt
+}
+
+
 ################################################################################
 # IPv4 UDP
 
@@ -3992,6 +4003,7 @@ do
 	ipv6_netfilter)  ipv6_netfilter;;
 
 	use_cases)       use_cases;;
+	tcp_authopt)     only_tcp_authopt;;
 
 	# setup namespaces and config, but do not run any tests
 	setup)		 setup; exit 0;;
