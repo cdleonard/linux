0f22704c3d2f9e829a2c13822c048feec1747bd8
Leonard Crestez <cdleonard@gmail.com>
2021-08-09 00:18:03 +0300

    tcp: authopt: Initial support and key management
    This commit adds support to add and remove keys but does not use them
    further.
    
    Similar to tcp md5 a single pointer to a struct tcp_authopt_info* struct
    is added to struct tcp_sock, this avoids increasing memory usage. The
    data structures related to tcp_authopt are initialized on setsockopt and
    only freed on socket close.
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -37,6 +37,7 @@
 #include <net/netns/smc.h>
 #include <net/netns/bpf.h>
 #include <net/netns/mctp.h>
+#include <net/netns/tcp_authopt.h>
 #include <net/net_trackers.h>
 #include <linux/ns_common.h>
 #include <linux/idr.h>
@@ -185,6 +186,9 @@ struct net {
 	struct sock		*diag_nlsk;
 #if IS_ENABLED(CONFIG_SMC)
 	struct netns_smc	smc;
+#endif
+#if IS_ENABLED(CONFIG_TCP_AUTHOPT)
+	struct netns_tcp_authopt	tcp_authopt;
 #endif
 } __randomize_layout;
 
