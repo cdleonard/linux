b58d745cc8fcc51a40c614e01d3aa201e7d623e1
Leonard Crestez <cdleonard@gmail.com>
2021-08-24 23:31:43 +0300

    selftests: tcp_authopt: Add tests for rollover
    RFC5925 requires that the use can examine or control the keys being
    used. This is implemented in linux via fields on the TCP_AUTHOPT
    sockopt.
    
    Add socket-level tests for the adjusting keyids on live connections and
    checking the they are reflected on the peer.
    
    Also check smooth transitions via rnextkeyid.
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tools/testing/selftests/tcp_authopt/tcp_authopt_test/linux_tcp_authopt.py
+++ tools/testing/selftests/tcp_authopt/tcp_authopt_test/linux_tcp_authopt.py
@@ -31,6 +31,8 @@ TCP_AUTHOPT_MAXKEYLEN = 80
 
 
 class TCP_AUTHOPT_FLAG(IntFlag):
+    LOCK_KEYID = BIT(0)
+    LOCK_RNEXTKEYID = BIT(1)
     REJECT_UNEXPECTED = BIT(2)
 
 
@@ -50,12 +52,20 @@ class tcp_authopt:
     """Like linux struct tcp_authopt"""
 
     flags: int = 0
-    sizeof = 4
+    send_keyid: int = 0
+    send_rnextkeyid: int = 0
+    recv_keyid: int = 0
+    recv_rnextkeyid: int = 0
+    sizeof = 8
 
     def pack(self) -> bytes:
         return struct.pack(
-            "I",
+            "IBBBB",
             self.flags,
+            self.send_keyid,
+            self.send_rnextkeyid,
+            self.recv_keyid,
+            self.recv_rnextkeyid,
         )
 
     def __bytes__(self):
@@ -63,7 +73,7 @@ class tcp_authopt:
 
     @classmethod
     def unpack(cls, b: bytes):
-        tup = struct.unpack("I", b)
+        tup = struct.unpack("IBBBB", b)
         return cls(*tup)
 
 
