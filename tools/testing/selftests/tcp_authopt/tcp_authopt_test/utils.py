8f33e6fecf72959920ee348d3edb3b74f81795f4
Leonard Crestez <cdleonard@gmail.com>
2021-09-19 23:30:12 +0300

    fixtest abi
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tools/testing/selftests/tcp_authopt/tcp_authopt_test/utils.py
+++ tools/testing/selftests/tcp_authopt/tcp_authopt_test/utils.py
@@ -2,37 +2,13 @@
 import json
 import random
 import subprocess
-import threading
-import typing
 import socket
-from dataclasses import dataclass
 from contextlib import nullcontext
 
 from nsenter import Namespace
-from scapy.sendrecv import AsyncSniffer
-
 
 # TCP port does not impact Authentication Option so define a single default
 DEFAULT_TCP_SERVER_PORT = 17971
-
-
-class SimpleWaitEvent(threading.Event):
-    @property
-    def value(self) -> bool:
-        return self.is_set()
-
-    @value.setter
-    def value(self, value: bool):
-        if value:
-            self.set()
-        else:
-            self.clear()
-
-    def wait(self, timeout=None):
-        """Like Event.wait except raise on timeout"""
-        super().wait(timeout)
-        if not self.is_set():
-            raise TimeoutError(f"Timed out timeout={timeout!r}")
 
 
 def recvall(sock, todo):
@@ -54,8 +30,12 @@ def randbytes(count) -> bytes:
     return bytes([random.randint(0, 255) for index in range(count)])
 
 
-def check_socket_echo(sock, size=1024):
-    """Send random bytes and check they are received"""
+def check_socket_echo(sock: socket.socket, size=1000):
+    """Send random bytes and check they are received
+
+    The default size is equal to `SimpleServerThread.DEFAULT_BUFSIZE` which
+    means that a single pair of packets will be sent at the TCP level.
+    """
     send_buf = randbytes(size)
     sock.sendall(send_buf)
     recv_buf = recvall(sock, size)
@@ -71,7 +51,7 @@ def nstat_json(command_prefix: str = ""):
         stdout=subprocess.PIPE,
         encoding="utf-8",
     )
-    return json.loads(runres.stdout)
+    return json.loads(runres.stdout)["kernel"]
 
 
 def netns_context(ns: str = ""):
@@ -102,53 +82,21 @@ def create_listen_socket(
     return listen_socket
 
 
-@dataclass
-class tcphdr_authopt:
-    """Representation of a TCP auth option as it appears in a TCP packet"""
-
-    keyid: int
-    rnextkeyid: int
-    mac: bytes
-
-    @classmethod
-    def unpack(cls, buf) -> "tcphdr_authopt":
-        return cls(buf[0], buf[1], buf[2:])
-
-    def __repr__(self):
-        return f"tcphdr_authopt({self.keyid}, {self.rnextkeyid}, bytes.fromhex({self.mac.hex(' ')!r})"
+def create_client_socket(
+    ns: str = "", family=socket.AF_INET, bind_addr="", bind_port=0, timeout=1.0
+):
+    with netns_context(ns):
+        client_socket = socket.socket(family, socket.SOCK_STREAM)
+    if bind_addr or bind_port:
+        client_socket.bind((str(bind_addr), bind_port))
+    if timeout is not None:
+        client_socket.settimeout(timeout)
+    return client_socket
 
 
-def scapy_tcp_get_authopt_val(tcp) -> typing.Optional[tcphdr_authopt]:
-    for optnum, optval in tcp.options:
-        if optnum == 29:
-            return tcphdr_authopt.unpack(optval)
-    return None
+def socket_set_linger(sock, onoff, value):
+    import struct
 
-
-def scapy_sniffer_start_block(sniffer: AsyncSniffer, timeout=1):
-    """Like AsyncSniffer.start except block until sniffing starts
-
-    This ensures no lost packets and no delays
-    """
-    if sniffer.kwargs.get("started_callback"):
-        raise ValueError("sniffer must not already have a started_callback")
-
-    e = SimpleWaitEvent()
-    sniffer.kwargs["started_callback"] = e.set
-    sniffer.start()
-    e.wait(timeout=timeout)
-
-
-def scapy_sniffer_stop(sniffer: AsyncSniffer):
-    """Like AsyncSniffer.stop except no error is raising if not running"""
-    if sniffer is not None and sniffer.running:
-        sniffer.stop()
-
-
-class AsyncSnifferContext(AsyncSniffer):
-    def __enter__(self):
-        scapy_sniffer_start_block(self)
-        return self
-
-    def __exit__(self, *a):
-        scapy_sniffer_stop(self)
+    sock.setsockopt(
+        socket.SOL_SOCKET, socket.SO_LINGER, struct.pack("ii", int(onoff), int(value))
+    )
