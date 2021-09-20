459d1e31c9817795735a215fa341f66a5316adc3
Leonard Crestez <cdleonard@gmail.com>
2021-09-20 20:42:13 +0300

    tcp: authopt: Disable via sysctl by default
    This is mainly intended to protect against local privilege escalations
    through a rarely used feature so it is deliberately not namespaced.
    
    Enforcement is only at the setsockopt level, this should be enough to
    ensure that the tcp_authopt_needed static key never turns on.
    
    No effort is made to handle disabling when the feature is already in
    use.
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -19,6 +19,7 @@
 #include <net/ping.h>
 #include <net/protocol.h>
 #include <net/netevent.h>
+#include <net/tcp_authopt.h>
 
 static int two = 2;
 static int three __maybe_unused = 3;
@@ -597,6 +598,15 @@ static struct ctl_table ipv4_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_do_static_key,
 	},
+#ifdef CONFIG_TCP_AUTHOPT
+	{
+		.procname	= "tcp_authopt",
+		.data		= &sysctl_tcp_authopt,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+#endif
 	{ }
 };
 
