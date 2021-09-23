389f835f98a413bc5e9ecc56f99983273b11d795
Leonard Crestez <cdleonard@gmail.com>
2021-09-23 21:45:25 +0300

    tcp: authopt: Add key selection controls
    The RFC requires that TCP can report the keyid and rnextkeyid values
    being sent or received, implement this via getsockopt values.
    
    The RFC also requires that user can select the sending key and that the
    sending key is automatically switched based on rnextkeyid. These
    requirements can conflict so we implement both and add a flag which
    specifies if user or peer request takes priority.
    
    Also add an option to control rnextkeyid explicitly from userspace.
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- include/net/tcp_authopt.h
+++ include/net/tcp_authopt.h
@@ -68,6 +68,39 @@ struct tcp_authopt_info {
 	u32 rcv_sne;
 	/** @snd_sne: Send-side Sequence Number Extension tracking tcp_sock.snd_nxt */
 	u32 snd_sne;
+
+	/**
+	 * @send_keyid: keyid currently being sent
+	 *
+	 * This is controlled by userspace by userspace if
+	 * TCP_AUTHOPT_FLAG_LOCK_KEYID, otherwise we try to match recv_rnextkeyid
+	 */
+	u8 send_keyid;
+	/**
+	 * @send_rnextkeyid: rnextkeyid currently being sent
+	 *
+	 * This is controlled by userspace if TCP_AUTHOPT_FLAG_LOCK_RNEXTKEYID is set
+	 */
+	u8 send_rnextkeyid;
+	/**
+	 * @recv_keyid: last keyid received from remote
+	 *
+	 * This is reported to userspace but has no other special behavior attached.
+	 */
+	u8 recv_keyid;
+	/**
+	 * @recv_rnextkeyid: last rnextkeyid received from remote
+	 *
+	 * Linux tries to honor this unless TCP_AUTHOPT_FLAG_LOCK_KEYID is set
+	 */
+	u8 recv_rnextkeyid;
+
+	/**
+	 * @send_key: Current key used for sending, cached.
+	 *
+	 * Once a key is found it only changes by user or remote request.
+	 */
+	struct tcp_authopt_key_info *send_key;
 };
 
 #ifdef CONFIG_TCP_AUTHOPT
@@ -84,7 +117,8 @@ struct tcp_authopt_key_info *__tcp_authopt_select_key(
 		const struct sock *sk,
 		struct tcp_authopt_info *info,
 		const struct sock *addr_sk,
-		u8 *rnextkeyid);
+		u8 *rnextkeyid,
+		bool locked);
 static inline struct tcp_authopt_key_info *tcp_authopt_select_key(
 		const struct sock *sk,
 		const struct sock *addr_sk,
@@ -95,7 +129,7 @@ static inline struct tcp_authopt_key_info *tcp_authopt_select_key(
 		*info = rcu_dereference(tcp_sk(sk)->authopt_info);
 
 		if (*info)
-			return __tcp_authopt_select_key(sk, *info, addr_sk, rnextkeyid);
+			return __tcp_authopt_select_key(sk, *info, addr_sk, rnextkeyid, true);
 	}
 	return NULL;
 }
