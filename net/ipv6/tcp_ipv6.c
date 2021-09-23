c76efc9d1326b7d6ccdc69e4b792a13add6b35ce
Leonard Crestez <cdleonard@gmail.com>
2021-09-23 21:45:25 +0300

    tcp: authopt: Add key selection controls
    The RFC requires that TCP can report the keyid and rnextkeyid values
    being sent or received, implement this via getsockopt values.
    
    The RFC also requires that user can select the sending key and that the
    sending key is automatically switched based on rnextkeyid. These
    requirements can conflict so we implement both and add a flag which
    specifies if user or peer request takes priority.
    
    Also add an option to control rnextkeyid explicitly from userspace.
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -916,7 +916,8 @@ static void tcp_v6_send_response(const struct sock *sk, struct sk_buff *skb, u32
 
 		if (authopt_info) {
 			authopt_key_info = __tcp_authopt_select_key(sk, authopt_info, sk,
-								    &authopt_rnextkeyid);
+								    &authopt_rnextkeyid,
+								    false);
 			if (authopt_key_info) {
 				tot_len += TCPOLEN_AUTHOPT_OUTPUT;
 				/* Don't use MD5 */
