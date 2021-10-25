e54f6a2c289c67872ef4bc65fa8d4b4ff22d2dc0
Leonard Crestez <cdleonard@gmail.com>
2021-10-25 22:49:44 +0300

    fixup! selftests: tcp_authopt: Initial sockopt manipulation
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tools/testing/selftests/tcp_authopt/tcp_authopt_test/conftest.py
+++ tools/testing/selftests/tcp_authopt/tcp_authopt_test/conftest.py
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0
 import logging
 import os
-from contextlib import ExitStack
+from contextlib import ExitStack, nullcontext
+from typing import ContextManager
 
 import pytest
 
-from .linux_tcp_authopt import has_tcp_authopt, enable_sysctl_tcp_authopt
+from .linux_tcp_authopt import enable_sysctl_tcp_authopt, has_tcp_authopt
 
 logger = logging.getLogger(__name__)
 
@@ -14,9 +15,23 @@ skipif_missing_tcp_authopt = pytest.mark.skipif(
 )
 
 
-def can_capture():
-    # This is too restrictive:
-    return os.geteuid() == 0
+def get_effective_capabilities():
+    for line in open("/proc/self/status", "r"):
+        if line.startswith("CapEff:"):
+            return int(line.split(":")[1], 16)
+
+
+def has_effective_capability(bit) -> bool:
+    return get_effective_capabilities() & (1 << bit) != 0
+
+
+def can_capture() -> bool:
+    return has_effective_capability(13)
+
+
+def raise_skip_no_netns():
+    if not has_effective_capability(12):
+        pytest.skip("Need CAP_NET_ADMIN for network namespaces")
 
 
 skipif_cant_capture = pytest.mark.skipif(
@@ -39,3 +54,18 @@ def pytest_configure():
     logging.getLogger("nsenter").setLevel(logging.INFO)
     if has_tcp_authopt():
         enable_sysctl_tcp_authopt()
+
+
+def parametrize_product(**kw):
+    """Parametrize each key to each item in the value list"""
+    import itertools
+
+    return pytest.mark.parametrize(",".join(kw.keys()), itertools.product(*kw.values()))
+
+
+def raises_optional_exception(expected_exception, **kw) -> ContextManager:
+    """Like pytest.raises except accept expected_exception=None"""
+    if expected_exception is None:
+        return nullcontext()
+    else:
+        return pytest.raises(expected_exception, **kw)
