6d65f5b0eefb1b4cba6498c8019c94debee17113
Leonard Crestez <cdleonard@gmail.com>
2021-11-04 14:14:46 +0200

    tcp: authopt: Fix accepting all keys with ids not configured locally
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- net/ipv4/tcp_authopt.c
+++ net/ipv4/tcp_authopt.c
@@ -1501,16 +1501,16 @@ out_err_traffic_key:
 static struct tcp_authopt_key_info *tcp_authopt_lookup_recv(struct sock *sk,
 							    struct sk_buff *skb,
 							    struct tcp_authopt_info *info,
-							    int recv_id)
+							    int recv_id,
+							    bool *anykey)
 {
 	struct tcp_authopt_key_info *result = NULL;
 	struct tcp_authopt_key_info *key;
 	int l3index = -1;
 
+	*anykey = false;
 	/* multiple matches will cause occasional failures */
 	hlist_for_each_entry_rcu(key, &info->head, node, 0) {
-		if (recv_id >= 0 && key->recv_id != recv_id)
-			continue;
 		if (key->flags & TCP_AUTHOPT_KEY_ADDR_BIND &&
 		    !tcp_authopt_key_match_skb_addr(key, skb))
 			continue;
@@ -1529,6 +1529,9 @@ static struct tcp_authopt_key_info *tcp_authopt_lookup_recv(struct sock *sk,
 			if (l3index != key->l3index)
 				continue;
 		}
+		*anykey = true;
+		if (recv_id >= 0 && key->recv_id != recv_id)
+			continue;
 		if (better_key_match(result, key))
 			result = key;
 		else if (result)
@@ -1564,6 +1567,7 @@ int __tcp_authopt_inbound_check(struct sock *sk, struct sk_buff *skb, struct tcp
 	struct tcphdr *th = (struct tcphdr *)skb_transport_header(skb);
 	struct tcphdr_authopt *opt;
 	struct tcp_authopt_key_info *key;
+	bool anykey;
 	u8 macbuf[TCP_AUTHOPT_MAXMACBUF];
 	int err;
 
@@ -1579,7 +1583,7 @@ int __tcp_authopt_inbound_check(struct sock *sk, struct sk_buff *skb, struct tcp
 		return -EINVAL;
 	}
 #endif
-	key = tcp_authopt_lookup_recv(sk, skb, info, opt ? opt->keyid : -1);
+	key = tcp_authopt_lookup_recv(sk, skb, info, opt ? opt->keyid : -1, &anykey);
 
 	/* nothing found or expected */
 	if (!opt && !key)
@@ -1589,7 +1593,7 @@ int __tcp_authopt_inbound_check(struct sock *sk, struct sk_buff *skb, struct tcp
 		print_tcpao_notice("TCP Authentication Missing", skb);
 		return -EINVAL;
 	}
-	if (opt && !key) {
+	if (opt && !anykey) {
 		/* RFC5925 Section 7.3:
 		 * A TCP-AO implementation MUST allow for configuration of the behavior
 		 * of segments with TCP-AO but that do not match an MKT. The initial
@@ -1603,6 +1607,12 @@ int __tcp_authopt_inbound_check(struct sock *sk, struct sk_buff *skb, struct tcp
 		}
 		print_tcpao_notice("TCP Authentication Unexpected: Accepted", skb);
 		goto accept;
+	}
+	if (opt && !key) {
+		/* Keys are configured for peer but with different keyid than packet */
+		NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPAUTHOPTFAILURE);
+		print_tcpao_notice("TCP Authentication Failed", skb);
+		return -EINVAL;
 	}
 
 	/* bad inbound key len */
