88dc10f47ab98d587e9bc8e5e947d49cbb2f6233
Leonard Crestez <cdleonard@gmail.com>
2022-07-14 18:57:56 +0300

    tcp: Refactor tcp_inbound_md5_hash into tcp_inbound_sig_hash
    The TCP-MD5 and TCP-AO signature options must be handled together so
    replace the old tcp_inbound_md5_hash with tcp_inbound_sig_hash which
    will handle both options.
    
    As a side effect of this change Linux will start dropping packets where
    both MD5 and AO are present instead of ignoring the so-far unrecognized
    AO option. This is a direct requirement from RFC5925 2.2
    
    This difference can be detected remotely without ever establishing a
    connection and used to fingerprint linux version. This seems acceptable.
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- include/net/dropreason.h
+++ include/net/dropreason.h
@@ -313,6 +313,10 @@ enum skb_drop_reason {
 	 */
 	SKB_DROP_REASON_FRAG_TOO_FAR,
 	/**
+	 * @SKB_DROP_REASON_TCP_BOTHAOMD5: Both AO and MD5 found in packet.
+	 */
+	SKB_DROP_REASON_TCP_BOTHAOMD5,
+	/**
 	 * @SKB_DROP_REASON_MAX: the maximum of drop reason, which shouldn't be
 	 * used as a real 'reason'
 	 */
